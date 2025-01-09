85e07e43707c24ee5808407e70d94a4472718257
finlay <finlaybob07@gmail.com>
2025-01-09 00:27:38 +0000

    Comments added to code explaining functions and logic

--- BoggleAssesment/Form1.cs
+++ BoggleAssesment/Form1.cs
@@ -13,21 +13,25 @@ namespace BoggleAssesment
 {
     public partial class Form1 : Form
     {
-        public string word1;
-        public int score;
+        public string word1; // Stores word entered by user
+        public int score; // Variable to track player score
 
         
-
+        // Event handler for "Enter" button click to validate and process the input word
         private void EnterStoffanddat_Click_1(object sender, EventArgs e)
         {
+            // Reads a list of valid words from the text file
             string[] DieList = System.IO.File.ReadAllLines(@"wordsList.txt");
             word1 = txtInputBox.Text;
 
            
-
+            // Checks if the entered word exists in the DieList array
             if (DieList.Contains(word1))
             {
+                // Displays a pop up message confirming the word is valid to the user
                 MessageBox.Show("Valid word", "Result");
+
+                // Resets the color of the labels to reset the selection after a correct word is entered
                 foreach (Control ctrl in this.Controls)
                 {
                     if (ctrl is Label label && label.Name.StartsWith("label"))
@@ -37,6 +41,7 @@ namespace BoggleAssesment
                     }
                 }
 
+                // Update score based on the word length and display it
                 score = word1.Length + score;
                 lblScore.Text = "Score : " + score.ToString();
 
@@ -44,9 +49,19 @@ namespace BoggleAssesment
             else
             {
 
+                // Update score based on the word length and display it
                 MessageBox.Show("Not valid", "Result");
+                foreach (Control ctrl in this.Controls)
+                {
+                    if (ctrl is Label label && label.Name.StartsWith("label"))
+                    {
+                        label.Enabled = true;
+                        label.ForeColor = Color.Black;
+                    }
+                }
 
             }
+            // Cleares the input box after the word is processed
             txtInputBox.Clear();
         }
 
@@ -65,7 +80,7 @@ namespace BoggleAssesment
             // Decrease remaining time by 1 second
             remainingTime--;
 
-            // Update the label to show remaining time in mm:ss format
+            // Update the label to show remaining time in minutes and seconds
             int minutes = remainingTime / 60;
             int seconds = remainingTime % 60;
             label100.Text = $"{minutes:D2}:{seconds:D2}";
@@ -81,16 +96,17 @@ namespace BoggleAssesment
                 {
                     if (ctrl is Label label && label.Name.StartsWith("label"))
                     {
-                        label.Enabled = false; // Disable all labels (no more input allowed)
-                        label.ForeColor = Color.Gray; // Optional: Change color to indicate disabled state
+                        label.Enabled = false; // Disable all labels (stops users selecting letters)
+                        label.ForeColor = Color.Gray; // Colour change to indicate a disabled state of the letters
                     }
                 }
 
-                // Optionally, you can disable the input box as well
+                // Disables textbox input
                 txtInputBox.Enabled = false;
             }
         }
 
+        // Function to return a random number between 0 and 5 (used in the dice selection)
         private int random()
         {
             Random rnd = new Random();
@@ -98,8 +114,11 @@ namespace BoggleAssesment
             return num;
         }
 
+        // Event handler for the form load event. Triggerd on start
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            // Arrays representing the dice faces for each dice
             string[] DIE0 = new string[] { "R", "I", "F", "O", "B", "X" };
             string[] DIE1 = new string[] { "I", "F", "E", "H", "E", "Y" };
             string[] DIE2 = new string[] { "D", "E", "N", "O", "W", "S" };
@@ -116,6 +135,8 @@ namespace BoggleAssesment
             string[] DIE13 = new string[] { "R", "A", "L", "E", "S", "C" };
             string[] DIE14 = new string[] { "U", "W", "I", "L", "R", "G" };
             string[] DIE15 = new string[] { "P", "A", "C", "E", "M", "D" };
+
+            // Randomly assign each label with a character from the dice array.
             label1.Text = DIE0[random()];
             label2.Text = DIE1[random()];
             label3.Text = DIE2[random()];
@@ -134,9 +155,10 @@ namespace BoggleAssesment
             label16.Text = DIE15[random()];
         }
 
+        // Refresh button functionality to reset the game
         private void Button1_Click(object sender, EventArgs e)
         {
-            //refresh button
+            // Arrays representing the possible letters on each dice
             string[] DIE0 = new string[] { "R", "I", "F", "O", "B", "X" };
             string[] DIE1 = new string[] { "I", "F", "E", "H", "E", "Y" };
             string[] DIE2 = new string[] { "D", "E", "N", "O", "W", "S" };
@@ -153,6 +175,8 @@ namespace BoggleAssesment
             string[] DIE13 = new string[] { "R", "A", "L", "E", "S", "C" };
             string[] DIE14 = new string[] { "U", "W", "I", "L", "R", "G" };
             string[] DIE15 = new string[] { "P", "A", "C", "E", "M", "D" };
+
+            // Randomly assign a letter from each dice to the labels (the ui dice)  (1 - 16)
             label1.Text = DIE0[random()];
             label2.Text = DIE1[random()];
             label3.Text = DIE2[random()];
@@ -170,11 +194,15 @@ namespace BoggleAssesment
             label15.Text = DIE14[random()];
             label16.Text = DIE15[random()];
 
+            // Restarts score
             score = 0;
             lblScore.Text = "Score: 0";
 
+            // Rests the timer and updates label
             remainingTime = 180; 
             label100.Text = "03:00"; 
+
+            // Starts the timer
             timer1.Start(); 
 
 
@@ -184,6 +212,7 @@ namespace BoggleAssesment
             // Clear the input box (if needed)
             txtInputBox.Clear();
 
+            // Enable all labels and set their color to black
             foreach (Control ctrl in this.Controls)
             {
                 if (ctrl is Label label && label.Name.StartsWith("label"))
@@ -195,9 +224,15 @@ namespace BoggleAssesment
             }
         }
 
+        // This function handles the click event for label1. When clicked, the text of label1 is appended to the input box,
+        // and adjacent labels are enabled for further selection. Non-adjacent labels are disabled to ensure the player only 
+        // selects neighboring dice. Enabled labels are highlighted in orange.
         private void Label1_Click(object sender, EventArgs e)
         {
+            // Add label1's text to the input box when clicked
             txtInputBox.Text += label1.Text;
+
+            // Disable all labels and enable only those that are adjacent to label1
             for (int i = 1; i <= 17; i++)
             {
                 string labelName = "label" + i.ToString();
@@ -207,14 +242,17 @@ namespace BoggleAssesment
                 // Check if the label is found before trying to access its properties
                 if (label != null)
                 {
+                    // Enable adjacent labels (orange color) and disable others
                     if (i == 2 || i == 5 || i == 6)
                     {
                         label.Enabled = true;
+                        // Highlight enabled labels in orange
                         label.ForeColor = Color.Orange;
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        // Disable non-adjacent labels
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -240,7 +278,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -265,7 +303,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false; 
                     }
                 }
             }
@@ -290,7 +328,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -308,7 +346,7 @@ namespace BoggleAssesment
                 
                 if (label != null)
                 {
-                    if (i == 1 || i == 2 || i == 6 || i == 9 || i == 10) //D
+                    if (i == 1 || i == 2 || i == 6 || i == 9 || i == 10) 
                     {
                         label.Enabled = true;
                         label.ForeColor = Color.Orange;
@@ -340,7 +378,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -365,7 +403,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -390,7 +428,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -415,7 +453,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -440,7 +478,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -465,7 +503,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -489,7 +527,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -514,7 +552,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false; //D
+                        label.Enabled = false; 
                     }
                 }
             }
@@ -539,7 +577,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -564,7 +602,7 @@ namespace BoggleAssesment
                     }
                     else
                     {
-                        label.Enabled = false;  //D
+                        label.Enabled = false;  
                     }
                 }
             }
@@ -595,6 +633,7 @@ namespace BoggleAssesment
             }
         }
 
+        // Exits the application when the Quit button is clicked
         private void Quit_Click(object sender, EventArgs e)
         {
             Application.Exit();
